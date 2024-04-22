c925a6b656926eb4268941398001e5f2186e5577
Siddesh <siddesh@ubuntu-22.04.3.myguest.virtualbox.org>
2024-04-22 13:30:21 +0530

    Changes to create similarity matrices for recommender system based on 3 criteria and to get recommendation given apartment name based on similarity matrix

---
+++ model/7_get_recommendation.py
@@ -0,0 +1,35 @@
+import pickle as pkl
+import argparse
+import numpy as np
+
+def fetchSimilar(apartment_name, criteria, k=5):
+    
+
+
+    with open('/home/siddesh/Desktop/Git_Repositories/Property_Price_Analysis_and_Prediction/model/similarity_'+criteria+'.pkl', 'rb') as file:
+        sim_matrix = pkl.load(file)
+
+    with open('/home/siddesh/Desktop/Git_Repositories/Property_Price_Analysis_and_Prediction/model/apartment_names.pkl', 'rb') as file:
+        apartment_names = pkl.load(file)
+
+    #print(apartment_names)
+    
+    index = apartment_names.to_list().index(apartment_name)
+    #print(f"Index is {index}")
+    #print(apartment_names.to_list())
+    sorted_indices = np.argsort(sim_matrix[index,:])
+    result = []
+    for idx in sorted_indices[0:k]:
+        print(apartment_names.to_list()[idx])
+        result.append(apartment_names.to_list()[idx])
+    return result
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Provide inputs to get similar apartment names based on criteria')
+    parser.add_argument('-apartment', type=str, required=True)
+    parser.add_argument('-criteria', type=str, choices = ['facilities', 'price_details', 'location_advantage'], required=True)
+    parser.add_argument('-top_k', type=int, default=5)
+
+    args = parser.parse_args()
+    fetchSimilar(args.apartment, args.criteria, k=args.top_k)
